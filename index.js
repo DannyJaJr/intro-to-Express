d95cfe01ca5d2591d81a3084bed0093035d8ae1c
Daniel Lafontant <ldanieljajr@gmail.com>
2021-05-05 11:06:05 -0400

    404Error

--- index.js
+++ index.js
@@ -1,56 +1,129 @@
-// //import
+// // //import
 
-// const express = require('express');
-// const app = express();
-// const axios = require('axios');
-
-
-
-// //SET UP HOME ROUTE 
-
-// app.get('/', function(req, res) {
-//     //ACCES DATABASE if needed
-//     //make some logic based off what is inside of request
-//     //access an API and return some data
-//     //send a respose at the end
-//     res.send('Hello, World!');
-// });
+// // const express = require('express');
+// // const app = express();
+// // const axios = require('axios');
 
 
-// //make an onother route here
-// app.get('/sei', (req, res) => {
-//     res.send('SEI 412');
-// })
-// app.get('/sei', sei);
 
-// app.get('/rockets', (req, res) => {
-//     //acees an API and return some data
-//     axios.get('https://api.spacexdata.com/v3/rockets')
-//     .then((response) => {
-//         //within this block of code
-//         //start
-//         console.log(response.data);
-//         // res.send('Rockets');
-//         res.json(response.data)
-//         //finish
-//     })
-//     .catch(error => {
-//         console.log(error);//no
-//     })
-    
-// })
+// // //SET UP HOME ROUTE 
 
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT, () => {
-//     console.log(`Server is listeneing on ${PORT}`)
-// });
-
-// //or it could be just    app.listen(8000);
-// // app.listen(8000, () => {
-// //     console.log(`Server is listening on Port 8000`);
+// // app.get('/', function(req, res) {
+// //     //ACCES DATABASE if needed
+// //     //make some logic based off what is inside of request
+// //     //access an API and return some data
+// //     //send a respose at the end
+// //     res.send('Hello, World!');
 // // });
 
 
+// // //make an onother route here
+// // app.get('/sei', (req, res) => {
+// //     res.send('SEI 412');
+// // })
+// // app.get('/sei', sei);
+
+// // app.get('/rockets', (req, res) => {
+// //     //acees an API and return some data
+// //     axios.get('https://api.spacexdata.com/v3/rockets')
+// //     .then((response) => {
+// //         //within this block of code
+// //         //start
+// //         console.log(response.data);
+// //         // res.send('Rockets');
+// //         res.json(response.data)
+// //         //finish
+// //     })
+// //     .catch(error => {
+// //         console.log(error);//no
+// //     })
+    
+// // })
+
+// // const PORT = process.env.PORT || 8000;
+// // app.listen(PORT, () => {
+// //     console.log(`Server is listeneing on ${PORT}`)
+// // });
+
+// // //or it could be just    app.listen(8000);
+// // // app.listen(8000, () => {
+// // //     console.log(`Server is listening on Port 8000`);
+// // // });
+
+
+
+
+
+// // Imports
+// const express = require('express');
+// const app = express(); // instance of the app
+// const axios = require('axios');
+// // Home route
+// app.get('/', function(req, res) {
+//     // access a database if needed
+//     // make some logic based off what is inside of request
+//     // access an API and return some data
+//     // send a response at the end
+//     res.send('Hello World!');
+// });
+// // const sei = (req, res) => {
+// //     res.send('SEI 412');
+// // }
+// // app.get('/sei', sei);
+
+  
+
+
+// app.get('/rockets', (req, res) => {
+//     // access an API and return some data
+//     axios.get('https://api.spacexdata.com/v3/rockets')
+//     .then((response) => {
+//         // within this block of code
+//         // ---- start -----
+//         console.log(response.data);
+//         res.json(response.data);
+//         // ---- finish -----
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+// })
+// //or app.get('/greet/:name', (req, res) => {
+// app.get('/:name', (req, res) => {
+//     console.log(req.params);
+//     res.send(`Hello ${req.params.name}`);
+//   });
+
+
+
+//       app.get('/github/:username', (req, res) => {
+//         const { username } = req.params; // destructuring
+//         axios.get(`https://api.github.com/users/${username}`)
+//         .then(response => {
+//             console.log(response.data);
+//             res.json(response.data);
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+//     });
+
+
+
+
+
+
+//     //import to be last , this is for any route not listed to return an 404 error
+// app.get('/*', (req, res) => {
+//     // console.log(req.prams);
+//     res.send(`404`);
+// });
+
+
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on ${PORT} 🎧`);
+// });
 
 
 
@@ -58,6 +131,7 @@
 const express = require('express');
 const app = express(); // instance of the app
 const axios = require('axios');
+
 // Home route
 app.get('/', function(req, res) {
     // access a database if needed
@@ -66,12 +140,14 @@ app.get('/', function(req, res) {
     // send a response at the end
     res.send('Hello World!');
 });
+
+
+
 // const sei = (req, res) => {
 //     res.send('SEI 412');
 // }
-// app.get('/sei', sei);
 
-  
+// app.get('/sei', sei);
 
 
 app.get('/rockets', (req, res) => {
@@ -88,30 +164,32 @@ app.get('/rockets', (req, res) => {
         console.log(error);
     })
 })
-//or app.get('/greet/:name', (req, res) => {
-app.get('/:name', (req, res) => {
-    console.log(req.params);
+
+app.get('/greet/:name', (req, res) => {
+    console.log(req.params); // req.params (object) => :name
     res.send(`Hello ${req.params.name}`);
-  });
+});
 
-
-
-      app.get('/github/:username', (req, res) => {
-        const { username } = req.params; // destructuring
-        axios.get(`https://api.github.com/users/${username}`)
-        .then(response => {
-            console.log(response.data);
-            res.json(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+app.get('/github/:username', (req, res) => {
+    const { username } = req.params; // destructuring
+    axios.get(`https://api.github.com/users/${username}`)
+    .then(response => {
+        console.log(response.data);
+        res.json(response.data);
+    })
+    .catch(error => {
+        console.log(error);
     });
+});
 
-
-
+// IMPORTANT: Make last route
+app.get('/*', (req, res) => {
+    
+    res.send('404');
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT} 🎧`);
 });
+
